9631e3ee843199d5ef68fb241381e7f01cda9c2d
Aksel Jänes <aksel.janes@khk.ee>
2015-09-13 19:47:41 +0300

    Teha kaks muutujat väärtustega 1.23 ja 2.34 ning kuvada ekraanil nende summat [Delivers #103062700]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -3,8 +3,12 @@
 $isik=(object) array('eesnimi'=>'Aksel', 'perenimi'=>'Jänes','vanus'=>20,'sugu'=>'Mees');
 
 $eesnimi_algab_vokaaliga=strstr("AEIOUÕÄÜ",$isik->{'eesnimi'}[0]);
-echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
+$arv1=1.23;
+$arv2=2.34;
+$summa=$arv1+$arv2;
 
+echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
+echo "Arvude ".$arv1." ja ".$arv2." summa on ".$summa."<br>";
 if($eesnimi_algab_vokaaliga){
     echo "Nimi algab vokaaliga";
 }
