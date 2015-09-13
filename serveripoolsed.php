805e44dad7fc04575baeaa8e3cec7dab2454b2f1
Aksel Jänes <aksel.janes@khk.ee>
2015-09-13 19:45:55 +0300

    Refaktoorida boolean tüüpi muutuja väärtus ümber literaalsest väärtusest dünaamiliseks, mis sõltub sellest, kas objekti 'eesnimi' atribuut tõepoolest algab vokaaliga või mitte [Delivers #103062704]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -2,7 +2,7 @@
 
 $isik=(object) array('eesnimi'=>'Aksel', 'perenimi'=>'Jänes','vanus'=>20,'sugu'=>'Mees');
 
-$eesnimi_algab_vokaaliga=true;
+$eesnimi_algab_vokaaliga=strstr("AEIOUÕÄÜ",$isik->{'eesnimi'}[0]);
 echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
 
 if($eesnimi_algab_vokaaliga){
