39b8137ddaf87ca15245463c30b6b89ba29c1c31
Aksel Jänes <aksel.janes@khk.ee>
2015-10-13 20:34:58 +0300

    Lisa .gitignore faili /config.php, et config.php kunagi git'i ei jõuaks. Loo fail config.php (märka, et PHPStorm ei küsi tema Git'i lisamise kohta). Lisa sinna 4 PHP konstanti: DATABASE_USERNAME, DATABASE_HOSTNAME, DATABASE_PASSWORD ja DATABASE_DATABASE. Pane neile kõigile oma MySQL serverisse ligi pääsemiseks vajalikud väärtused. Kopeeri fail config.php failiks config.sample.php (märka, et PHPStorm küsib tema Git'i lisamise kohta ja vasta JAH). Muuda config.sample.php failis parool tühja väärtusega stringiks. Kaasa index.php faili alguses require käsuga config.php fail. Kasuta index.php's andmebaasiga ühendumiseks config.phps määratud konstante. Testi et töötab. Commiti. [Delivers #105587594]

--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,5 @@
 <?php
-    define('DATABASE_HOSTNAME','127.0.0.1');
+    define('DATABASE_HOSTNAME','localhost');
     define('DATABASE_USERNAME','root');
     define('DATABASE_PASSWORD','root');
     define('DATABASE_DATABASE','cleanblog');
