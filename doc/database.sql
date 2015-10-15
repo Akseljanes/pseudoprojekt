005e9f4f794438d06a9cb490eb17aa20d23b2efb
Aksel Jänes <aksel.janes@khk.ee>
2015-10-15 13:24:11 +0300

    Mine Githubi projektilehele, vajuta settings ja webhooks ja lisa uus webhook aadressile http://diarainfra.com/khk/deploy.php?group=vs14&user=GITHUBUSER&project=PROJEKT, kus GITHUBUSER on sinu GitHub kasutajanimi ja PROJEKT on antud projekti nimi githubis, näiteks pseudoprojekt. Kustuta failid kliendipoolsed.php ja serveripoolsed.php. Commiti ja pushi. Mine aadressile http://diarainfra.com/khk/vs14/GITHUBUSER/PROJEKT ja vaata, et vigu poleks. Kui on, siis lisa Pivotal Trackerisse bug tüüpi töö vea tekstiga ja lahenda ära (guugelda veateadet, aga ilma muutuvate osadeta nagu failinimed ja faili rea numbrid). Viimasena lisa http://tinyurl.com/pwvxcod tabelisse oma diarainfra deploy link (http://diarainfra.com/khk/vs14/GITHUBUSER/PROJEKT) [Delivers #105589340]

--- doc/database.sql
+++ doc/database.sql
@@ -1,15 +1,96 @@
-CREATE TABLE authors
-(
-    author_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    author_name VARCHAR(255) NOT NULL
-);
-CREATE TABLE posts
-(
-    post_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    author_id INT UNSIGNED NOT NULL,
-    post_title VARCHAR(255) NOT NULL,
-    post_description VARCHAR(255) NOT NULL,
-    post_text LONGTEXT NOT NULL,
-    post_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES authors (author_id)
-);
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 22, 2015 at 01:13 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `$posts`
+--
+
+CREATE TABLE IF NOT EXISTS `$posts` (
+  `post_id` int(10) unsigned NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `description` varchar(120) NOT NULL,
+  `author_id` int(10) unsigned NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `$posts`
+--
+
+INSERT INTO `$posts` (`post_id`, `title`, `description`, `author_id`, `date`, `content`) VALUES
+(1, 'Man must explore, and this is exploration at its greatest', 'Problems look mighty small from 150 miles up', 1, '2014-08-24 00:00:00', ''),
+(2, 'lalalalala', 'llalalalalalalal', 1, '2014-08-24 00:00:00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE IF NOT EXISTS `authors` (
+  `authors_id` int(10) unsigned NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`authors_id`, `name`) VALUES
+(1, 'Start Bootstrap');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `$posts`
+--
+ALTER TABLE `$posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`authors_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `$posts`
+--
+ALTER TABLE `$posts`
+  MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `authors_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
