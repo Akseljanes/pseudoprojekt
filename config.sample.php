96d764cfec3b11197ef21679dc0130247dc1ffe8
Aksel Jänes <aksel.janes@khk.ee>
2015-10-15 13:53:19 +0300

    Mine Githubi projektilehele, vajuta settings ja webhooks ja lisa uus webhook aadressile http://diarainfra.com/khk/deploy.php?group=vs14&user=GITHUBUSER&project=PROJEKT, kus GITHUBUSER on sinu GitHub kasutajanimi ja PROJEKT on antud projekti nimi githubis, näiteks pseudoprojekt. Kustuta failid kliendipoolsed.php ja serveripoolsed.php. Commiti ja pushi. Mine aadressile http://diarainfra.com/khk/vs14/GITHUBUSER/PROJEKT ja vaata, et vigu poleks. Kui on, siis lisa Pivotal Trackerisse bug tüüpi töö vea tekstiga ja lahenda ära (guugelda veateadet, aga ilma muutuvate osadeta nagu failinimed ja faili rea numbrid). Viimasena lisa http://tinyurl.com/pwvxcod tabelisse oma diarainfra deploy link (http://diarainfra.com/khk/vs14/GITHUBUSER/PROJEKT) [Delivers #105589340]

--- config.sample.php
+++ config.sample.php
@@ -2,5 +2,5 @@
     define('DATABASE_HOSTNAME','localhost');
     define('DATABASE_USERNAME','root');
     define('DATABASE_PASSWORD','root');
-    define('DATABASE_DATABASE','blog');
+    define('DATABASE_DATABASE','cleanblog');
 ?>
